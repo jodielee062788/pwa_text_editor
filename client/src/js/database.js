bb2531c57f6522576ba4be7afee0eb8b7a22dc6e
Jodie <jodielee062788@gmail.com>
2024-04-06 16:02:21 +1100

    Create a function to get all the data from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,22 +13,45 @@ const initdb = async () =>
   });
 
 // Function to add content to the database
-export const putDb = async (content) => 
-  console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    // Open the database
+    const textDb = await openDB('jate', 1);
 
-  // Open the database
-  const textDb = await openDB('jate', 1);
+    // Start a transaction and get the object store
+    const tx = textDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
 
-  // Start a transaction and get the object store
-  const tx = textDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+    // Add the data to the object store
+    const request = store.add({ content: content });
+    const result = await request;
+    console.log('Data saved to the database', result);
 
-  // Add the data to the object store
-  const request = store.add({ content: content });
-  const result = await request;
-  console.log('Data saved to the database', result);
+  } catch (error) {
+    console.error('Error posting data to the database:', error);
+    }
+  };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    // Open the database
+    const textDb = await openDB('jate', 1);
+
+    // Start a read-only transaction and get the object store
+    const tx = textDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+
+    // Retrieve all data from the object store
+    const request = store.getAll();
+
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+    
+  } catch (error) {
+    console.error('Error retrieving data from the database:', error);
+    }
+  };
 
 initdb();
