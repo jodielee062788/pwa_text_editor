d66cf30407ec8b7392e6fde0072fa97710d412d7
Jodie <jodielee062788@gmail.com>
2024-04-06 15:53:36 +1100

    Create a function to add data to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Function to add content to the database
+export const putDb = async (content) => 
+  console.error('putDb not implemented');
+
+  // Open the database
+  const textDb = await openDB('jate', 1);
+
+  // Start a transaction and get the object store
+  const tx = textDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  // Add the data to the object store
+  const request = store.add({ content: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
