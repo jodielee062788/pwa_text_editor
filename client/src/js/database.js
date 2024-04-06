fdd530063427ef60640165394568bb177a6041e0
Jodie <jodielee062788@gmail.com>
2024-04-06 16:03:05 +1100

    Add comments to explain how the code works

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -32,7 +32,7 @@ export const putDb = async (content) => {
     }
   };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Function to retrieve content from the database
 export const getDb = async () => {
   try {
     // Open the database
@@ -48,7 +48,7 @@ export const getDb = async () => {
     const result = await request;
     console.log('result.value', result);
     return result;
-    
+
   } catch (error) {
     console.error('Error retrieving data from the database:', error);
     }
